88a8e353f486495a81708292d7aef8d5bded2f5c
Luke Russell <thelukerussel21@gmail.com>
2020-01-24 14:06:11 -0500

    created a sample repo

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
 
-  const URL = 'https://api.github.com/users';
-  const USER = 'TheLukeRussell';
-  const USER_URL = 'https://api.github.com/users/thelukerussell'
+  const BASE_URL = 'https://api.github.com/users';
+  const USER = 'thetukerussell';
+  const CLIENT_ID = 'ac4982b5e60293a2e5c1'
+  const CLIENT_SECRET = 'bbd445fe98af3d6d4c283efa15d1e48ac3d954f3'
+
   // const GITHUB_API_KEY = '775e45a998307284b1a9616dc6004bb1bffdea8f'
   // console.log(GITHUB_API_KEY);
+
   $.ajax({
-    url: 'https://api.github.com/users/thelukerussell',
-    Authorization: `token ${GITHUB_API_KEY}`
+    url: `https://api.github.com/users/thelukerussell?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`,
     })
     .done((resp) => {
-      updateOrgs(resp);
+      // updateOrgs(resp);
     })
     .fail((err) => {
       console.log(err);
@@ -25,14 +27,14 @@ $(document).ready(function() {
 
   $.ajax('https://api.github.com/users/thelukerussell').done((user) => {
     // console.log(info);
-    let bioInfo = [{
+    let bioInfo = {
       avatar: user.avatar_url,
       login: user.login,
       location: user.location,
       name: user.name,
       updated_at: user.updated_at,
       organizatons_url: user.organizatons_url
-    }]
+    }
     console.log(bioInfo);
     displayBio(bioInfo);
 
@@ -43,6 +45,26 @@ $(document).ready(function() {
     let template = Handlebars.compile(source)
     $('#bio').html(template(bioInfo));
   };
+
+  $.ajax('https://api.github.com/users/thelukerussell/repos').done((data) => {
+    console.log(data);
+    displayRepos(repoList);
+  })
+
+let displayRepos = (repoList) => {
+  let source = $('repo-template').html();
+  let template = Handlebars.compile(source);
+
+
+  $('#repoList').html(template(repo));
+};
+
+let displayOrgs = (orgInfo) => {
+  let source = $('org-temp').html();
+  let template = Handlebars.compile(source);
+
+  $('#org-list').html(template(repo));
+}
 
 
   // let context = {
